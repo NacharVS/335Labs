56cfde0dd213099e66044e940389a35e6a7681c3
r3dinc <r3dinc@yandex.ru>
2020-10-13 08:21:34 +0300

    1

--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -6,7 +6,8 @@ namespace _335Labs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
         }
     }
 }
