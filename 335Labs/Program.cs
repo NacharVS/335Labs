d02de0dd261cfd7b5175f67b500658c917bb7b8c
batinov <batinov-nikita@mail.ru>
2020-10-15 11:08:20 +0300

    12

--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -6,8 +6,8 @@ namespace _335Labs
     {
         static void Main(string[] args)
         {
-            string a = Console.ReadLine();
-            Console.WriteLine(a);
+            Console.WriteLine("Hello Nikita");
+           
         }
     }
 }
