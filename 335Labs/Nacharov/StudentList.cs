969c1305779bb9abf81380dc5d31ebf3cc18c1da
r3dinc <r3dinc@yandex.ru>
2020-10-15 09:58:30 +0300

    finish

---
+++ 335Labs/Nacharov/StudentList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _335Labs.Nacharov
+{
+    class StudentList
+    {
+        public static void sum(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+    }
+}
--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -1,13 +1,12 @@
-﻿using System;
+﻿using _335Labs.Nacharov;
+using System;
 
 namespace _335Labs
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            string a = Console.ReadLine();
-            Console.WriteLine(a);
+        { 
         }
     }
 }
