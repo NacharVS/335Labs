051a9137fb2c5bb5d3d3466ef3220d80501dee26
alkir202 <user12367642@gmail.com>
2020-10-15 22:44:29 +0300

    Added circum ference programm
    nope

---
+++ 335Labs/Kirillov/Circum_Ference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Kirillov
+{
+    class Circum_Ference1
+    {
+        static public double CircumFerence(double r)
+        {
+            double pi = 3.1415;
+            return pi * r * r;
+        }
+    }
+}
--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -1,12 +1,18 @@
-﻿using _335Labs.Nacharov;
-using System;
+﻿using System;
 
 namespace _335Labs
 {
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            Console.WriteLine("Введите радиус круга: ");
+            double r = double.Parse(Console.ReadLine());
+
+            double res = Kirillov.Circum_Ference1.CircumFerence(r);
+            Console.WriteLine($"S круга равна: ");
+            Console.WriteLine(res);
+
         }
     }
 }
