80872a9d2fd602cd150d1cd04b195f68e765ab61
RugdBab <haruharu09@mail.ru>
2020-10-16 09:28:47 +0300

    eee

---
+++ 335Labs/Khabibrakhmanov/Metods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Gilmutdinov
+{
+    class _111
+    {
+        public static void Main()
+        {
+            int a = -5; 
+            int b = 5;
+            int c = 10;
+
+        }
+        
+    }
+    static void Sum(int a, int b, int c)
+
+    {
+        int res;
+        for (int i = a; i <= b; i++)
+        {
+            res = c / a;
+            Console.WriteLine(res);
+        }
+       
+    }
+}
