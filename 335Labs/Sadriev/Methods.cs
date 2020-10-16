3b7f536ccde7f399a4395a8b99ce5dca49b36258
SadrievRinat <ris2002@mail.ru>
2020-10-16 09:28:19 +0300

    end of para

--- 335Labs/Sadriev/Methods.cs
+++ 335Labs/Sadriev/Methods.cs
@@ -6,7 +6,82 @@ namespace _335Labs.Sadriev
 {
     class Methods
     {
+        public static int DivofInt(int a, int b, int c)
+        {
+            int res = 0;
+            for (int i = a; i <= b; i++)
+            {
+                if (i == 0)
 
+                {
+                    continue;
+
+                }
+                res = c / i;
+                Console.WriteLine(res);
+            }
+            return 0;
+
+
+        }
+        public static (double, double) xx(double a, double b, double c)
+        {
+            double D = b * b - 4 * a * c;
+            if (D >= 0)
+            {
+
+                double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+
+                return (x1, x2);
+            }
+            else Console.WriteLine("korney net");
+            return (0, 0);
+        }
+    
+            
+
+        
+        public static double Pif(double a, double b)
+        {
+            double d = a * a + b * b;
+            double c = Math.Sqrt(d);
+            return c;
+        }
+        public static double Circum(double r)
+        {
+            double c = 2 * 3.14 * r;
+
+            
+            return c;
+        }
+        public static double CirArea(double r)
+        {
+            double c = 3.14 * r * r;
+
+            ;
+            return c;
+            
+        }
+        public static double ParVol(double a, double b, double c)
+        {
+            double res = a * b * c;
+
+            return res;
+        }
+        public static double AVR(double a, double b)
+        {
+
+
+
+
+            return 0;
+        
+        
+        }
+
+        
+            
 
 
 
