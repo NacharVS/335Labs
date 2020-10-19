adab517892658662a1a4d21675b0995abb113d37
Adel <gilmutdinov.adel2013@yandex.ru>
2020-10-19 22:58:50 +0300

    Start

---
+++ 335Labs/Gilmutdinov/strings1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Gilmutdinov
+{
+    class @strings1
+    {
+       public static string First(string a)
+        {
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+
+                    Console.Write(a[i] + "        ");
+                }
+                else if (i % 2 != 0)
+                    Console.WriteLine(a[i]);
+
+            }
+            return " ";
+        }
+
+        public static string Second(string a)
+        {
+            
+        }
+
+    }
+}
--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -7,17 +7,19 @@ namespace _335Labs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Methotds.Sum(-5,5,10));
-            Console.WriteLine(Methotds.Disc(2,10,-4));
-            Console.WriteLine(Methotds.Pif(2, 4));
-            Console.WriteLine(Methotds.Circircumference(2));
-            Console.WriteLine(Methotds.AreaCir(2));
-            Console.WriteLine(Methotds.Volume(5, 5, 10));
-            Console.WriteLine(Methotds.Amperage(10, 5));
-            Console.WriteLine(Methotds.Voltage(2, 5));
-            Console.WriteLine(Methotds.Resistance(2, 10));
-
+            //Console.WriteLine(Methotds.Sum(-5, 5, 10));
+            //Console.WriteLine(Methotds.Disc(2, 10, -4));
+            //Console.WriteLine(Methotds.Pif(2, 4));
+            //Console.WriteLine(Methotds.Circircumference(2));
+            //Console.WriteLine(Methotds.AreaCir(2));
+            //Console.WriteLine(Methotds.Volume(5, 5, 10));
+            //Console.WriteLine(Methotds.Amperage(10, 5));
+            //Console.WriteLine(Methotds.Voltage(2, 5));
+            //Console.WriteLine(Methotds.Resistance(2, 10));
+            string a = "Best on the best";
+            Console.WriteLine((strings1.First(a)));
+            Console.WriteLine((strings1.Second(a)));
         }
     }
-}
+}   
 
