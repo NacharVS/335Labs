b0d11be5f9c343c3f79113456577f85547451f3c
Ibrahim329 <mrshagaviev@gmail.com>
2020-10-19 12:50:34 +0300

    seredina

--- 335Labs/Shagaviev Barygin/chetnechet.cs
+++ 335Labs/Shagaviev Barygin/chetnechet.cs
@@ -5,6 +5,21 @@ using System.Text;
 namespace _335Labs.Shagaviev_Barygin
 {
     class chetnechet
-    {
+    { 
+         public static void Chisl()
+        {
+            string word = "вводное слово";
+            int first = -1;
+            int last = -1;
+            foreach (char ch in word)
+            {
+                first = word.IndexOf(ch);
+                last = word.LastIndexOf(ch);
+                if (first != -1 && last != -1 && first != last)
+                    word = word.Replace(ch.ToString(), "");
+            }
+            Console.Write(word);
+            Console.ReadKey();
+        }
     }
 }
