df882140aae9bef68810bc4701c9a4f9a5ab7a44
trunkkker <starstalker19@gmail.com>
2020-10-19 13:18:42 +0300

    Added new folders

---
+++ 335Labs/Girfanova/Program.cs
@@ -0,0 +1,47 @@
+﻿using _335Labs.GirfanovaBakhmutova;
+using System;
+
+
+namespace _335Labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MathMetods
+            {
+                // Testing
+
+                // ax^2-bx+c
+                // FindRoot(1, 2, 3, 4);
+
+
+                //c^2=a^2+b^2
+                // Pyth(20, 30);
+
+
+                // Circumference
+                // Circumference(5);
+
+
+                // Circle's area
+                // AreaCirc(5);
+
+
+                // Parallelepiped's area
+                // VolParall(2, 3, 2);
+
+
+                // amperage/voltage/resistance
+                // CurrentCount(0, 5, 15);
+
+
+                // Finds result of division number by an interval of numbers
+                // Division(15, 1, 15);
+                // Console.WriteLine();
+            }
+
+            //
+        }
+    }
+}
