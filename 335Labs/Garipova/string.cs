57b7da00d40f64dbefeabcc49823124df0bcd198
GulyuzaGaripova <gulyuza.garipova.00@mail.ru>
2020-10-19 13:08:29 +0300

    start

---
+++ 335Labs/Garipova/string.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Garipova
+{
+    class @string
+    {
+        public static void Concatenation(string s);
+       
+    }
+
+}
