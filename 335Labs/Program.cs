8bbf128acf66f13efd70f726fcac71144f74ea56
MarV <351806@mck-ktits.ru>
2020-10-19 13:18:34 +0300

    nnnihao

--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -1,4 +1,4 @@
-﻿using _335Labs.Nacharov;
+﻿
 using System;
 
 namespace _335Labs
@@ -6,7 +6,14 @@ namespace _335Labs
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            string[] string_letters = 33433
+                public static void Concatenation(string string_letter)
+                {
+                    Console.WriteLine(string.Concat(string_letter));
+                }
+
+            }
+        
         }
-    }
 }
--- 335Labs/Solodov/MathMetod.cs
+++ 335Labs/Solodov/MathMetod.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace _335Labs.Solodov
 {
     class MathMetod
     {
+     
     }
 }
