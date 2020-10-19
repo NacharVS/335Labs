87d263d80848403f7c9a5f4890b7d29e786e4091
Nadezhda-ts <nadya.zagaynova.99@mail.ru>
2020-10-19 13:19:42 +0300

    End of pair

---
+++ 335Labs/Zagaynova/stringss.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Zagaynova
+{
+    class @stringss
+    {
+        public static (string,string) Raspr( string s)
+        {
+            string Even = null;
+            string Odd = null;
+            for (int i = 0; i <=s.Length ; i++)
+                
+            { int d = i % 2;
+
+                if
+                 (d == 0) { Even = Even + s[i]; }
+
+                else { Odd += +s[i]; }
+             }
+            return (Even, Odd);
+           
+
+       
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
