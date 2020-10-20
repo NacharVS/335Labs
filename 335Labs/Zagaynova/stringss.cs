c1ea5dc28c7ed0ef2b95d884fe89e4ec488d9e0a
Nadezhda-ts <nadya.zagaynova.99@mail.ru>
2020-10-20 08:32:52 +0300

    1 right

--- 335Labs/Zagaynova/stringss.cs
+++ 335Labs/Zagaynova/stringss.cs
@@ -4,30 +4,53 @@ using System.Text;
 
 namespace _335Labs.Zagaynova
 {
-    class @stringss
+    class @Stringss
     {
-        public static (string,string) Raspr( string s)
+        public static void Raspr(string s)
+
         {
-            string Even = null;
+            string  Even = null;
             string Odd = null;
-            for (int i = 0; i <=s.Length ; i++)
+
+
+            for (int i = 0; i < s.Length; i++)
                 
             { int d = i % 2;
 
-                if
-                 (d == 0) { Even = Even + s[i]; }
+                if (d == 0)
+                {
+                    Even += s[i];
+                }
 
-                else { Odd += +s[i]; }
+                else if (d!= 0)
+                {
+                    Odd += s[i]; 
+                    
+                }
              }
-            return (Even, Odd);
+            Console.WriteLine(Even);
+            Console.WriteLine(Odd);
+
+         }
+
+
+        public static void Del(string s)
+        {
            
 
-       
+            for (int i = 0; i < s.Length; i++)
 
+
+            {
+                for (int j= 0; j < s.Length; j++)
+                { 
+                    if (s[i]==s[j]) { s.Remove(s[i],1); }
+                }
+
+            }
+            Console.WriteLine(s);
         }
-
-
-
+    
 
 
 
