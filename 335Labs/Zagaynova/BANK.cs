ba3839711e2c965328ea6d4edf6f21bf6931f452
Nadezhda-ts <nadya.zagaynova.99@mail.ru>
2020-10-22 21:22:34 +0300

    Homework

--- 335Labs/Zagaynova/BANK.cs
+++ 335Labs/Zagaynova/BANK.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -10,27 +11,47 @@ namespace _335Labs.Zagaynova
         private string _name;
         private string _surname;
         private string _id;
-     
+
         private double _paymentAccount;
 
-        public  void Registration(string newName , string newSurname)
+        public void Registration(string newName, string newSurname)
         {
-            newName= newName.Trim();
+            newName = newName.Trim();
             var first = newName[0];
             var other = newName.Remove(0, 1);
             _name = first.ToString().ToUpper() + other.ToLower();
-            newSurname = newName.Trim();
-            var firsts = newName[0];
-            var others = newName.Remove(0, 1);
-            _surname = first.ToString().ToUpper() + other.ToLower();
+            newSurname = newSurname.Trim();
+            var firsts = newSurname[0];
+            var others = newSurname.Remove(0, 1);
+            _surname = firsts.ToString().ToUpper() + others.ToLower();
             Random rnd = new Random();
             int a = rnd.Next(1000, 9999);
             _id = a.ToString();
 
             Console.WriteLine($" id {_id} Name {_name} Surname {_surname}");
         }
+        public void PaymentAc(int a ,string s)
+            //положить минимум 10 000
+            //снять максимум 200 000
+        {if (s == "Sum")
+            {
+                _paymentAccount = 0;
+                if (a< 10000) { Console.WriteLine("minimum amount 10 000"); }
+                else _paymentAccount += +a;
+            }
+        else if (s=="Sub")
+            {
+                _paymentAccount = 0;
+                if (a > 200000) { Console.WriteLine("maximum amount 200 000"); }
+                else _paymentAccount = _paymentAccount - a;
+            }
+         }
 
+        public void PaymentAc()
+        {
+            Console.WriteLine(_paymentAccount);
 
+        }
 
     }
 }
