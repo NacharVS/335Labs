d3e403b57c5d4ee47ab047a506e377b1de14fd2f
trunkkker <starstalker19@gmail.com>
2020-10-22 23:57:49 +0300

    starting BankAccount

---
+++ 335Labs/Bakhmutova/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs
+{
+    class BankAccount
+    {
+        private string _name;
+        private string _surname;
+        private string _id;
+        private string _paymentAccount;
+
+        public void Registration(string n, string s)
+        {
+            n=n.Trim();
+            s = s.Trim();
+            var firstLet = n[0];
+            var otherLet = n.Remove(0,1);
+            _name = n.ToString().ToUpper() + s.ToString().ToUpper();
+            Random rn = new Random();
+            int id = rn.Next(1000, 9999);
+            Console.WriteLine($"id: { id}");
+       //     int k = 0;
+           // foreach () { }
+        }
+    }
+}
