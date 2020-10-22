8c28a15fcc950e250c73d34a323f6e8d0fe41f72
GulyuzaGaripova <gulyuza.garipova.00@mail.ru>
2020-10-22 10:53:40 +0300

    finish

--- 335Labs/Garipova/string.cs
+++ 335Labs/Garipova/string.cs
@@ -5,9 +5,62 @@ using System.Text;
 namespace _335Labs.Garipova
 {
     class @string
+
+
     {
-        public static void Concatenation(string s);
-       
+        public static void Odd_Even(string s)
+        {
+
+            Console.WriteLine("Odd characters");
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ') continue;
+                if (i%2==0)
+                    Console.WriteLine(s[i]);
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Even charakters");
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ') continue;
+
+                if (i % 2 ! = 0)
+                    Console.WriteLine(s[i]);
+            }
+
+        }
+
+
+        public static void countwords (string s)
+        {
+            string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"Count of words per line = { words.Length}");
+
+        }
+
+
+        public static void Palindrome(string s)
+        {
+
+            s = s.Replace(" ", "");
+            int count = 0;
+            for (int i =0; i <s.Length; i++)
+            {
+                if (s[i] == s[s.Length - 1 - i])
+                    count += 1;
+
+            }
+            if (count == s.Length)
+                Console.WriteLine("This string is a palendrome");
+            else
+                Console.WriteLine("This string is not a palendrome");
+        }
+
     }
+
+   
 
 }
