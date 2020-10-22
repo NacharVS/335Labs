909493ddf6f76930c486d95d1bfce0728ef370db
Ibrahim329 <mrshagaviev@gmail.com>
2020-10-22 10:43:32 +0300

    SEREDINA

--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -8,7 +8,7 @@ namespace _335Labs
     {
         static void Main(string[] args)
         {
-            udalenie.Simvol(); 
+            BankAccount.UserAcc(); 
         }
     }
 }
--- 335Labs/Shagaviev Barygin/BankAccount.cs
+++ 335Labs/Shagaviev Barygin/BankAccount.cs
@@ -7,8 +7,20 @@ namespace _335Labs.Shagaviev_Barygin
     class BankAccount
     {
         public static void UserAcc()
+        { }
+        private string _name;
+        private string _surname;
+        private string _id;
+        private Double _PaymentAccount; 
+        
+        public void SetName(string newName)
         {
-            private 
+            newName = newName.Trim();
+            var firstLetter = newName[0];
+            var otherLetters = newName.Remove(0, 1);
+            _name = firstLetter.ToString().ToUpper() + otherLetters;
         }
+
+            
     }
 }
