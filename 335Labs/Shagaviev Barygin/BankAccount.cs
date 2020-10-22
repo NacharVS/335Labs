d9dfff4fbe4f63eac6122b136b0dfe9bef7f12a3
Ibrahim329 <mrshagaviev@gmail.com>
2020-10-22 10:31:17 +0300

    start 2

---
+++ 335Labs/Shagaviev Barygin/BankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Shagaviev_Barygin
+{
+    class BankAccount
+    {
+        public static void UserAcc()
+        {
+            private 
+        }
+    }
+}
