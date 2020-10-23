0f4bda3ebd28385039eabd7fefc15340b71fa2b4
Khasanov335 <KhasanovFarit@mail.ru>
2020-10-23 09:29:55 +0300

    23

--- 335Labs/Khusaenov/list.cs
+++ 335Labs/Khusaenov/list.cs
@@ -4,8 +4,73 @@ using System.Text;
 
 namespace _335Labs.Khusaenov
 {
-    class list
+    class BankAccount
     {
+        private string _name;
+        private string _surname;
+        private string _bank;
+        private double _id;
+        public static double _rate;
+        public string _paymentAccount;
 
+        public void SetName(string newName)
+        {
+            newName = newName.Trim();
+            var firstletter = newName[0];
+            var otherletters = newName.Remove(0, 1);
+            _name = firstletter.ToString().ToUpper() + otherletters;
+        }
+
+        public void SetSurname(string newSurname)
+        {
+            newSurname = newSurname.Trim();
+            var firstletter = newSurname[0];
+            var otherletters = newSurname.Remove(0, 1);
+            _surname = firstletter.ToString().ToUpper() + otherletters;
+
+        }
+
+        public void SetBank(string newBank)
+        {
+            newBank = newBank.Trim();
+            var firstletter = newBank[0];
+            var otherletters = newBank.Remove(0, 1);
+            _bank = firstletter.ToString().ToUpper() + otherletters;
+
+        }
+
+        public void SetId(int newID)
+        {
+            Random rnd = new Random();
+            int[] ID = new int[1];
+            for (int i = 0; i < ID.Length; i++)
+            {
+                ID[i] = rnd.Next(0, 1000);
+            }
+            _id = newID;
+        }
+
+        public void SetRate(double Rate)
+        {
+            Random rmd = new Random();
+            double[] newRate = new double[1];
+            for (int i = 0; i < newRate.Length; i++)
+            {
+                newRate[i] = rmd.Next(0, 100);
+            }
+            _rate = Rate;
+        }
+
+        public void SetPaymentAccount(string newPaymentAccount)
+        {
+
+            _paymentAccount = newPaymentAccount;
+
+        }
+
+       
     }
-}
+
+    
+} 
+
--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -1,12 +1,22 @@
-﻿using _335Labs.Nacharov;
+﻿using _335Labs.Khusaenov;
 using System;
 
 namespace _335Labs
 {
     class Program
     {
+
         static void Main(string[] args)
-        { 
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine("Введите Имя");
+            account.SetName(Console.ReadLine());
+            Console.WriteLine("Введите Фамилию");
+            account.SetSurname(Console.ReadLine());
+            Console.WriteLine("Название Банка");
+            account.SetBank(Console.ReadLine());
+            Console.WriteLine();
+            
         }
     }
 }
