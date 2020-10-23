3010c9af3d7bdbf675f09a9d9c558ce7edc8e474
Ibrahim329 <mrshagaviev@gmail.com>
2020-10-23 09:16:53 +0300

    start2finish

---
+++ 335Labs/Shagaviev Barygin/Bank.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _335Labs.Shagaviev_Barygin
+{ }
+public class Bank
+{
+    private void OpenNewAccount(Client client)
+    {
+        var account = new Account
+        {
+
+            Balance = ReadBalance("Введите баланс")
+
+        };
+        client.CreateAccount(account);
+        Console.WriteLine($"Клиент №{client.ID} {client.FullName} был успешно добавлен");
+    }
+
+    private object ReadBalance(string v)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class Client
+{
+    public object FullName { get; internal set; }
+}
+
+internal class Account
+{
+    public object Balance { get; set; }
+}
