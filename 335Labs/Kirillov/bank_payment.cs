56b3805c2c9635a61f46c905f45091b3cba38da1
alkir202 <user12367642@gmail.com>
2020-10-23 09:13:35 +0300

    Added bank account class

---
+++ 335Labs/Kirillov/bank_payment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Kirillov
+
+{
+    class bank_acc
+    {
+        private string _name;
+        private string _lastname;
+        private string _id;
+
+        public void Bank_Account(string newn, string newsur)
+        {
+            newn = newn.Trim();
+            var firstLetter = newn[0];
+            var otherLetters = newn.Remove(0, 1);
+            _name = firstLetter.ToString().ToUpper() + otherLetters;
+            newsur = newsur.Trim();
+            var firstLetterr = newsur[0];
+            var otherLetterrs = newsur.Remove(0, 1);
+            _lastname = firstLetterr.ToString().ToUpper() + otherLetterrs;
+            Console.WriteLine(newsur);
+            Random rnd = new Random();
+            int a = rnd.Next(888, 88888888);
+            _id = $"{a}";
+            Console.WriteLine($"Имя: {_name} Фамилия: {_lastname}");
+            Console.WriteLine($"#ID: {a}");
+        }
+    }
+}
+    
--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace _335Labs
+namespace _335Labs.Kirillov
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите ваше имя: ");
+            string newn = Console.ReadLine();
+            Console.WriteLine("Введите вашу фамилию: ");
+            string newsur = Console.ReadLine();
 
-            
+            bank_acc bankacc = new bank_acc();
+            bankacc.Bank_Account(newn, newsur);
+
         }
     }
 }
