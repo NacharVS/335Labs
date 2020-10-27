c6791fe9f1ed5d88c836a3cddab3f61b0ee4ab44
batinov <batinov-nikita@mail.ru>
2020-10-27 09:07:52 +0300

    continue work

--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -2,31 +2,131 @@
 
 namespace _335Labs
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            division(1, 5, 10);
-        }
-        static void division(double a, int b, int c)
-        {
-            double result = 0;
-            for (int i = b; i < c; i++) 
+            Bank<Account> bank = new Bank<Account>("ЮнитБанк");
+            bool alive = true;
+            while (alive)
             {
-                result = a / i;
-                Console.WriteLine(result);
-            }
-        }
-        static void Discriminant(double a,int b,int c)
-        {
-            double result = 0;
-            double x = 1;
-            for (int i = b; i>a;i++)
-            {
-                result = a * x * x - b * x + c;
+                ConsoleColor color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkGreen; // выводим список команд зеленым цветом
+                Console.WriteLine("1. Открыть счет \t 2. Вывести средства  \t 3. Добавить на счет");
+                Console.WriteLine("4. Закрыть счет \t 5. Пропустить день \t 6. Выйти из программы");
+                Console.WriteLine("Введите номер пункта:");
+                Console.ForegroundColor = color;
+                try
+                {
+                    int command = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine(result);
+                    switch (command)
+                    {
+                        case 1:
+                            OpenAccount(bank);
+                            break;
+                        case 2:
+                            Withdraw(bank);
+                            break;
+                        case 3:
+                            Put(bank);
+                            break;
+                        case 4:
+                            CloseAccount(bank);
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            alive = false;
+                            continue;
+                    }
+                    bank.CalculatePercentage();
+                }
+                catch (Exception ex)
+                {
+                    // выводим сообщение об ошибке красным цветом
+                    color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ForegroundColor = color;
+                }
             }
-        }       
+        }
+
+        private static void OpenAccount(Bank<Account> bank)
+        {
+            Console.WriteLine("Укажите сумму для создания счета:");
+
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Выберите тип счета: 1. До востребования 2. Депозит");
+            AccountType accountType;
+
+            int type = Convert.ToInt32(Console.ReadLine());
+
+            if (type == 2)
+                accountType = AccountType.Deposit;
+            else
+                accountType = AccountType.Ordinary;
+
+            bank.Open(accountType,
+                sum,
+                AddSumHandler,  // обработчик добавления средств на счет
+                WithdrawSumHandler, // обработчик вывода средств
+                (o, e) => Console.WriteLine(e.Message), // обработчик начислений процентов в виде лямбда-выражения
+                CloseAccountHandler, // обработчик закрытия счета
+                OpenAccountHandler); // обработчик открытия счета
+        }
+
+        private static void Withdraw(Bank<Account> bank)
+        {
+            Console.WriteLine("Укажите сумму для вывода со счета:");
+
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Введите id счета:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            bank.Withdraw(sum, id);
+        }
+
+        private static void Put(Bank<Account> bank)
+        {
+            Console.WriteLine("Укажите сумму, чтобы положить на счет:");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Введите Id счета:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            bank.Put(sum, id);
+        }
+
+        private static void CloseAccount(Bank<Account> bank)
+        {
+            Console.WriteLine("Введите id счета, который надо закрыть:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            bank.Close(id);
+        }
+        // обработчики событий класса Account
+        // обработчик открытия счета
+        private static void OpenAccountHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        // обработчик добавления денег на счет
+        private static void AddSumHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        // обработчик вывода средств
+        private static void WithdrawSumHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+            if (e.Sum > 0)
+                Console.WriteLine("Идем тратить деньги");
+        }
+        // обработчик закрытия счета
+        private static void CloseAccountHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
