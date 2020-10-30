ee88a51ec5b72f5cfbd85ac0efe3f8627c3b2b5a
Ibrahim329 <mrshagaviev@gmail.com>
2020-10-30 11:02:41 +0300

    finish

---
+++ 335Labs/Shagaviev Barygin/ClassAbstr.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Shagaviev_Barygin
+abstract class Person
+{
+    public string Name { get; set; }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void Display();
+}
+
+class Client : Person
+{
+    public int Sum { get; set; }    // сумма на счету
+
+    public Client(string name, int sum)
+        : base(name)
+    {
+        Sum = sum;
+    }
+    public override void Display()
+    {
+        Console.WriteLine($"{Name} имеет счет на сумму {Sum}");
+    }
+}
+
+class Employee : Person
+{
+    public string Position { get; set; } // должность
+
+    public Employee(string name, string position)
+        : base(name)
+    {
+        Position = position;
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine($"{Position} {Name}");
+    }
