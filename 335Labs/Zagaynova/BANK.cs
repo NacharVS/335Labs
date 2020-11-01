abd73324185299944a37ae913340cce3a29ff8f6
Nadezhda-ts <nadya.zagaynova.99@mail.ru>
2020-11-01 15:00:05 +0300

    Finish

--- 335Labs/Zagaynova/BANK.cs
+++ 335Labs/Zagaynova/BANK.cs
@@ -8,21 +8,20 @@ using System.Text;
 
 namespace _335Labs.Zagaynova
 {
-    class BANK
+    abstract class BANK
     {
         private string _name;
         private string _surname;
         private string _id;
         private int _age;
 
-        private double _paymentAccount;
-        private static double _Stavka= 0.067;
 
-        public string Registration(string newName, string newSurname,int yy, int mm, int dd)
+
+        public string Registration(string newName, string newSurname, int yy, int mm, int dd)
         {
             int aa = DateTime.Now.Year;
-            DateTime date = new DateTime(yy,mm,dd);
-           newName = newName.Trim();
+            DateTime date = new DateTime(yy, mm, dd);
+            newName = newName.Trim();
             var first = newName[0];
             var other = newName.Remove(0, 1);
             _name = first.ToString().ToUpper() + other.ToLower();
@@ -33,48 +32,20 @@ namespace _335Labs.Zagaynova
             Random rnd = new Random();
             int a = rnd.Next(1000, 9999);
             _id = a.ToString();
-
             _age = aa - date.Year;
             if (date > DateTime.Now.AddYears(-_age)) Age--;
-
-
-
             return "";
-         }
+        }
 
         public int Age
-        { get
+        {
+            get
             { return _age; }
             set
             { _age = value; }
-        }   
-         
-        public double PaymentAc(int a, string s)
-        //положить минимум 10 000
-        //снять максимум 200 000
-        {
-
-            if (s == "Sum")
-            {
-
-                if (a < 10000) { Console.WriteLine("minimum amount 10 000"); }
-                else _paymentAccount += +a;
-            }
-            else if (s == "Sub")
-            {
-
-                if (a > 200000) { Console.WriteLine("maximum amount 200 000"); }
-                else _paymentAccount = _paymentAccount - a;
-            }
-            
-            return _paymentAccount;
         }
 
-        public void PaymentAc()
-        {
-            Console.WriteLine(_paymentAccount);
 
-        }
         public string Edit(string rname, string rsurname)
         {
             rname = rname.Trim();
@@ -85,32 +56,92 @@ namespace _335Labs.Zagaynova
             var firsts = rsurname[0];
             var others = rsurname.Remove(0, 1);
             _surname = firsts.ToString().ToUpper() + others.ToLower();
-            
-           
             string reakk = _id + " " + _name + " " + _surname;
-
             return reakk;
-        }
-
+         }
         public string Info()
-
         {
-            string ifoss = _id + " " + _name + " " + _surname  + "\n"+ "Money in the account:" + _paymentAccount + "\n " + "age:" +_age;
+            string ifoss = _id + " " + _name + " " + _surname + "\n"  +  "age:" + _age;
             return ifoss;
-        }   
-           public double  Proc()
-        {
-            _paymentAccount +=  _paymentAccount * _Stavka;
+        }
 
-            return _paymentAccount;
-        }
-        public static void ReProc(double newProc)
-        {
-            _Stavka = newProc / 100;
-        }
+    }
+      class Client : BANK 
+      {
+            private double _paymentAccount;
+            private static double _Stavka = 0.067;
+
+            public double PaymentAc(int a, string s)
+            //положить минимум 10 000
+            //снять максимум 200 000
+            {
+                if (s == "Sum")
+                {
+                    if (a < 10000) { Console.WriteLine("minimum amount 10 000"); }
+                    else _paymentAccount += +a;
+                }
+                else if (s == "Sub")
+                {
+                    if (a > 200000) { Console.WriteLine("maximum amount 200 000"); }
+                    else _paymentAccount = _paymentAccount - a;
+                }
+                  return _paymentAccount;
+            }
+            public void PaymentAc()
+            {
+                Console.WriteLine(_paymentAccount);
+            }
             
+            public double Proc()
+            {
+                _paymentAccount += _paymentAccount * _Stavka;
+                 return _paymentAccount;
+            }
+            public static void ReProc(double newProc)
+            {
+                _Stavka = newProc / 100;
+            }
+
+            public void ShowInfo()
+            {
+            string inf = Info() + "\n" + "money in the account" + _paymentAccount;
+            Console.WriteLine(inf);
+            }
+       }
+    class Employee : BANK
+    {
+        private string _post;
+        public string  Post
+        {
+            get
+            { return _post; }
+            set
+            { _post = value; }
+        }
+        public void ShowInfo()
+        {
+            string inf = Info() + "\n" + "Post-" + _post;
+            Console.WriteLine(inf);
+        }
     }
 
 
-}
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+    
