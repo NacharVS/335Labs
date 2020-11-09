d699949a52a44c788896b4abb81628089d696926
Ibrahim329 <mrshagaviev@gmail.com>
2020-11-09 13:08:25 +0300

    Mid

--- 335Labs/Shagaviev Barygin/Delegate/BrOffice.cs
+++ 335Labs/Shagaviev Barygin/Delegate/BrOffice.cs
@@ -6,5 +6,39 @@ namespace _335Labs.Shagaviev_Barygin.Delegate
 {
     class BrOffice
     {
+        static void Main(string[] args)
+        {
+
+            int[] nums = new int[7];
+            Console.WriteLine("Введите семь чисел");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0}-е число: ", i + 1);
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+
+
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+
+            Console.WriteLine("Вывод отсортированного массива");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            Console.ReadLine();
+        }
     }
 }
