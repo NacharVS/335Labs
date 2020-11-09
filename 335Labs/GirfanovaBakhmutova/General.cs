d555077ecbe831c9fe45d4d2505ce86c8ee7c13b
trunkkker <starstalker19@gmail.com>
2020-11-09 13:15:44 +0300

    starting general (delegates)

---
+++ 335Labs/GirfanovaBakhmutova/General.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.GirfanovaBakhmutova
+{
+    class General
+    {
+        //int sum = 0;
+        //sum += mas[i];
+        //mas[i] = mas[i] / sum;
+
+        //сумма одномерного массива
+        public delegate void General1();
+        int[] mas;
+
+    }
+}
