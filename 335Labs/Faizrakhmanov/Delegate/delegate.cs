f0fb4934d60cd713a106b18861d47dad063d74e7
alkir202 <user12367642@gmail.com>
2020-11-09 13:20:04 +0300

    hello

---
+++ 335Labs/Faizrakhmanov/Delegate/delegate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Faizrakhmanov.Delegate
+{
+    class @delegate
+    {
+        Console.WriteLine("privet");
+    }
+}
