538532ceb7239d2291434827579d6a8f0f7f5bd2
RugdBab <haruharu09@mail.ru>
2020-11-09 13:13:47 +0300

    Ne tak uzh i nuzhno

--- 335Labs/Khabibrakhmanov/Delegated/Boss.cs
+++ 335Labs/Khabibrakhmanov/Delegated/Boss.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace _335Labs.Khabibrakhmanov.Deligated
 {
+   
     class Boss
     {
     }
---
+++ 335Labs/Khabibrakhmanov/Delegated/Filla.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Khabibrakhmanov.Deligated
+{
+    class Filla
+    {
+        public static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+            Random rand = new Random();
+
+            for (int x = 0; x < myArray.Length; x++)
+            {
+                myArray[x] = rand.Next(20);
+                Console.WriteLine("Значение элемента массива " + x + " = " + myArray[x]);
+            }
+        }
+    }
+    static int Sortirovka()
+    {
+
+
+    }
+    static int Summa()
+        }
+
+}
+
