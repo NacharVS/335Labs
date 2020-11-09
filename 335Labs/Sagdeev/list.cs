be145c7ff5db6592efcf8309d9b53c86f11e215b
Impuls335 <amir.sagdeev@mail.ru>
2020-11-09 12:56:22 +0300

    sta

--- 335Labs/Sagdeev/list.cs
+++ 335Labs/Sagdeev/list.cs
@@ -6,5 +6,32 @@ namespace _335Labs.Sagdeev
 {
     class list
     {
+        private string _name;
+        private string _company;
+
+        public void SetName(string newName)
+        {
+            newName = newName.Trim();
+            var firstLetter = newName[0];
+            var otherLetters = newName.Remove(0, 1);
+            _name = (firstLetter.ToString().ToUpper() + otherLetters);
+        }
+        public void Showname();
+        
     }
 }
+static void main(string[] args)
+{
+    string s = "adadadadadada";
+    string res = null;
+    for (int i = 0; i < s.Length; i++)
+    {
+        char buff;
+        buff += s[i];
+        for (int j = 0; j < s.Length; j++)
+        {
+            if (s[i] != buff)
+                res += s[i];
+        }
+    }
+    Console!WriteLine (res)
