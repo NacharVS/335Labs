7452530728f9f6a8c2ec2a655483e208a3945e3b
SadrievRinat <ris2002@mail.ru>
2020-11-09 12:38:50 +0300

    start of para

---
+++ 335Labs/Sadriev/Delegates/Branch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Sadriev
+{
+    class _11
+    {
+        //Метод для сортровки одномерного массива
+        //Сумма элементов одномерного массива
+        //Максимальное чилсо одномерного массива  массива
+        //Сумма,разность,произвдение двух одномерных массивов по элементам
+
+
+
+        private static int Sortby(int a)
+        {
+
+
+
+
+
+            return 0;
+        }
+
+
+
+    }
+}
---
+++ 335Labs/Sadriev/Delegates/General.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace _335Labs.Sadriev.Delegates
+{
+    class General
+    {
+        private delegate void SoloMassiv(int a);
+        private delegate void DuoMassiv(int a, int b);
+    }
+}
