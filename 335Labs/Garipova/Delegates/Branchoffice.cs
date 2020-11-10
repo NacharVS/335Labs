7bd969e7823497d85ada66604df19039504a0d33
GulyuzaGaripova <gulyuza.garipova.00@mail.ru>
2020-11-10 09:28:05 +0300

    end on a pair

--- 335Labs/Garipova/Delegates/Branchoffice.cs
+++ 335Labs/Garipova/Delegates/Branchoffice.cs
@@ -6,14 +6,38 @@ namespace _335Labs.Garipova.Delegates
 {
     class Branchoffice
     {
-        private delegate void Worker(string message);
+        private delegate void Array(string message);
 
         static void Main(string[] args)
         {
-            Worker wkr;
-            wkr = Generaloffice.Worker;
-            wkr("Branch office");
+            int n, summa = 0;
+            Random rnd = new Random();
+            Console.WriteLine("find the dimension");
+            double n = double.Parse(Console.ReadLine());
+            int[,] a = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = rnd.Next(-10, 10);
+                    Console.WriteLine(a[i, j] + "");
+                    summa = summa + a[i, j];
+                }
+
+            }
+
+        }
+        private delegate void array1(string message);
+
+        private static void Main1(string[] args)
+        {
+            int a, summa = 0;
+            int[] numbers = new int[] { 0, 1, 2, 3, 4, 5 };
+
 
         }
     }
 }
+
+
+
