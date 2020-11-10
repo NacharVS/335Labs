4b78b77d903659c352ac5b43aaa745b06ad758c3
Nadezhda-ts <nadya.zagaynova.99@mail.ru>
2020-11-10 09:17:20 +0300

    Finish

--- 335Labs/Zagaynova/delegates/BranchOffice.cs
+++ 335Labs/Zagaynova/delegates/BranchOffice.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace _335Labs.Zagaynova.delegates
 {
     class BranchOffice
     {
+        public delegate void One(int[] mas);
+        public delegate void Two(int[] mast, int[] mass);
+            
+      
     }
 }
--- 335Labs/Zagaynova/delegates/MainOffice.cs
+++ 335Labs/Zagaynova/delegates/MainOffice.cs
@@ -11,54 +11,61 @@ namespace _335Labs.Zagaynova.delegates
 
         {
             Array.Sort(mas);
+            Console.Write("sort:");
             for (int i = 0; i < mas.Length; i++)
             {
-                Console.WriteLine(mas[i]);
+                Console.Write(mas[i] +  " ");
             }
         }
         public static void Max(int[] mas)
         {
             int max = mas.Max();
-            Console.WriteLine(max);
+            Console.WriteLine( "max:"+  max);
 
         }
         public static void Summa (int []mas)
         {
             int sum = mas.Sum();
-            Console.WriteLine(sum);
+            Console.WriteLine("sum:" , sum);
         }
         public static void SummaDva(int[] masOne, int[] masTwo)
         {
+            Console.Write("SumDva:");
 
-            int[] sumdva= new int [5];
-            for (int i = 0; i < sumdva.Length; i++)
+            
+            for (int i = 0; i < masOne.Length; i++)
             {
               int n = masOne[i] + masTwo[i];
                 Console.Write(n+ " ");
             }
-            
+            Console.WriteLine();
+
         }
         public static void SubDva(int[] masOne, int[] masTwo)
         {
+            Console.Write("SubDva:");
 
-            int[] sumdva = new int[5];
-            for (int i = 0; i < sumdva.Length; i++)
+           
+            for (int i = 0; i < masOne.Length; i++)
             {
                 int n = masOne[i] - masTwo[i];
                 Console.Write(n + " ");
             }
+            Console.WriteLine();
 
         }
         public static void ComDva(int[] masOne, int[] masTwo)
         {
+            Console.Write("ComDva:");
 
-            int[] sumdva = new int[5];
-            for (int i = 0; i < sumdva.Length; i++)
+          
+            for (int i = 0; i < masOne.Length; i++)
             {
                 int n = masOne[i] * masTwo[i];
                 Console.Write(n + " ");
             }
+            Console.WriteLine();
 
-        }
+        } 
     } 
 }
