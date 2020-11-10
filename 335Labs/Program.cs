95d79ecdad758c12c79d04e532696ec725ae3a36
Impuls335 <amir.sagdeev@mail.ru>
2020-11-10 09:06:37 +0300

    Start

--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -1,12 +1,39 @@
-﻿using _335Labs.Nacharov;
-using System;
+﻿using System;
 
-namespace _335Labs
+namespace ConsoleApp20
 {
-    class Program
+    class ClassesAndObjects
     {
-        static void Main(string[] args)
-        { 
+        int sum = 0;
+        public void TheArray()
+        {
+            int[] sum = new int[5];
+            int[,] array = new int[5, 5];
+            Random rnd = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    array[i, j] = rnd.Next(0, 10);
+                    Console.Write(array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < array.GetLength(0); ++i) 
+            {
+                for (int j = 0; j < array.GetLength(0); ++j) 
+                {
+                    sum[i] += array[i, j];
+                }
+                Console.WriteLine(sum[i]);
+            }
+            Array.Sort(sum);
+            Console.WriteLine();
+            for (int i = 0; i < sum.GetLength(0); i++) 
+            {
+                Console.WriteLine(sum[i]);
+            }
         }
     }
 }
