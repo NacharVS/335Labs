d5f2952b7c293124c22fe1bc4a6a9d481abf21ab
alkir202 <user12367642@gmail.com>
2020-11-12 20:01:54 +0300

    homework1

--- 335Labs/Kirillov/Delegates/delegate1.cs
+++ 335Labs/Kirillov/Delegates/delegate1.cs
@@ -5,9 +5,9 @@ using System.Linq;
 
 namespace _335Labs.Kirillov.Delegates
 {
-    namespace DelgateForNotes
+    namespace Delgate
     {
-        class Program
+        class delegate1
         {
             public delegate int AddDelegate(int[] mas);
 
@@ -16,13 +16,12 @@ namespace _335Labs.Kirillov.Delegates
                 int[] mas = new int[] { 1, 2, 3, 4, 5 };
                 AddDelegate del1 = new AddDelegate(Sum);
                 AddDelegate del2 = new AddDelegate(Max);
-                AddDelegate del3 = new AddDelegate(Sort);
+                
                 int a = del1(mas);
                 int b = del2(mas);
-                int d = del3(mas);
+                
                 Console.WriteLine("Сумма = {0}", a);
                 Console.WriteLine("Макс. число = {0}", b);
-                Console.WriteLine("Сортировка массива = {0}", d);
                 Console.Read();
             }
 
@@ -39,16 +38,9 @@ namespace _335Labs.Kirillov.Delegates
                 return sumation;
 
             }
-            public static int Sort(int[] mas)
-            {
-                Array.Sort(mas);
-                for (int i = 0; i < mas.Length; i++)
-                {
-                    return mas[i];
 
-                }
-                
-            }
+
+            
         }
     }
 }
