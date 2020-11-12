4110923c3e318de34e7456b47b6984db46f3bdcf
aigir <5047098196@edu.tatar.tu>
2020-11-12 22:32:00 +0300

    General arrays (need to continue work)

---
+++ 335Labs/Girfanova/General.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace _335Labs.Girfanova
+{
+    public static class General
+    {
+
+        public delegate void MathOA(int[] arr);
+        public delegate void MathTA(int[] arr1, int[] arr2);
+
+        public static void SortOA(Array arr)
+        {
+            Array.Sort(arr);
+            Console.WriteLine(arr);
+        }
+
+        public static void SumOA(int[] arr)
+        {
+            int sum = 0;
+            foreach (int value in arr)
+            {
+                sum += value;
+            }
+            Console.WriteLine(sum);
+        }
+
+        public static void FindMaxOA(int[] arr)
+        {
+            Array.Sort(arr);
+            int max = arr[arr.Length - 1];
+            Console.WriteLine(max);
+        }
+
+        public static void SumTA(int[] arr1, int[] arr2)
+        {
+            int[] arr3 = new int[0];
+            for (int r = 0; r < arr1.Length; ++r)
+            {
+                arr3[r] = arr1[r] + arr2[r];
+            }
+            Console.WriteLine(arr3);
+        }
+
+        public static void SubTA(int[] arr1, int[] arr2)
+        {
+            int[] arr3 = new int[0];
+            for (int r=0; r<arr1.Length; ++r)
+            {
+                arr3[r] = arr1[r] - arr2[r];
+            }
+            Console.WriteLine(arr3);
+        }
+
+        public static void MulTA(int[] arr1, int[] arr2)
+        {
+            int[] arr3 = new int[0];
+            for (int r = 0; r < arr1.Length; ++r)
+            {
+                arr3[r] = arr1[r] * arr2[r];
+            }
+            Console.WriteLine(arr3);
+        }
+
+    }
+}
---
+++ 335Labs/Girfanova/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static _335Labs.Girfanova.MathMetods;
+using static _335Labs.Girfanova.General;
+
+namespace _335Labs.Girfanova
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            // Testing MathMetods
+
+            // ax^2-bx+c
+            //FindRoot(1, 2, 3, 4);
+
+
+            //c^2=a^2+b^2
+            // Pyth(20, 30);
+
+
+            // Circumference
+            // Circumference(5);
+
+
+            // Circle's area
+            // AreaCirc(5);
+
+
+            // Parallelepiped's area
+            // VolParall(2, 3, 2);
+
+
+            // amperage/voltage/resistance
+            // CurrentCount(0, 5, 15);
+
+
+            // Finds result of division number by an interval of numbers
+            // Division(15, 1,15);
+            //Console.ReadLine();
+            //________________________________________________________________
+
+
+            MathOA array = SortOA;
+            array = [1, 0, 16, 9, 77 ];
+        }
+    }
+}
