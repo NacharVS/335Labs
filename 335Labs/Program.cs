ef18fc09f436169281df57252c8ebd1691164e0e
MarV <351806@mck-ktits.ru>
2020-11-13 09:19:01 +0300

    Vitalya is Ponchick/ I'm ponchic

--- 335Labs/Program.cs
+++ 335Labs/Program.cs
@@ -1,19 +1,1 @@
 ﻿
-using System;
-
-namespace _335Labs
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string[] string_letters = 33433
-                public static void Concatenation(string string_letter)
-                {
-                    Console.WriteLine(string.Concat(string_letter));
-                }
-
-            }
-        
-        }
-}
--- 335Labs/Solodov/Test.cs
+++ 335Labs/Solodov/Test.cs
@@ -2,9 +2,46 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _335Labs.Solodov
+
+namespace _335Labs.Gayfullin
 {
-    class Test
+    class MathMetods
     {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Добрый день, введите ID-номер вашего счета и выберите операцию: 1 - снять со счета. 2 - положить на счет. 3 - посмотреть информацию о счете. 4 - депозит.");
+            int a = int.Parse(Console.ReadLine());
+            int q = int.Parse(Console.ReadLine());
+            int b = 40000;
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Укажите сумму, которую вы хотите снять со счета:");
+                    int c = int.Parse(Console.ReadLine());
+                    if (c < 5000)
+                        b = b - c;
+                    break;
+                case 2:
+                    Console.WriteLine("Укажите сумму, которую вы хотите положить на счет:");
+                    int v = int.Parse(Console.ReadLine());
+                    if (v < 12000)
+                        b = b + v;
+                    break;
+                case 3:
+                    Console.WriteLine("Информация о вашем счете:");
+                    Console.WriteLine("ID-номер счета :");
+                    Console.WriteLine($"{q}");
+                    Console.WriteLine("Сумма денег на счету:");
+                    Console.WriteLine($"{b}");
+                    break;
+                case 4:
+                    Console.WriteLine("Введите сумму депозита(не более 20000, не менее 4000:");
+                    int g = int.Parse(Console.ReadLine());
+                    break;
+            }
+        }
+
+
+
     }
 }
