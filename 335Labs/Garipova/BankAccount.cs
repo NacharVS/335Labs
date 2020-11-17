cc068c3d250305ede383d7f73b611d62dc24458f
GulyuzaGaripova <gulyuza.garipova.00@mail.ru>
2020-11-17 09:29:53 +0300

    on a pair

--- 335Labs/Garipova/BankAccount.cs
+++ 335Labs/Garipova/BankAccount.cs
@@ -1,11 +1,91 @@
-﻿using System;
+﻿using Stripe;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace _335Labs.Garipova
 {
-    class BankAccount
+    public class BankAccount
     {
-        private string Joldus;
-    }
+        public delegate void SumChanged(string phonenumber, int sum);
+        public delegate void RateChanged(double oldRate, double newRate);
+        private string _name;
+        private string _surname;
+        private string _id;
+        private static double _rate = 0.35;
+        private DateTime accountOpenDate;
+        private double _paymentAccount;
+        private int _sum;
+        private string _phoneNumber;
+        public BankAccount(int sum, string phonenumber)
+        {
+            _sum = sum;
+            _phoneNumber = phonenumber;
+        }
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            BankAccount acc1 = new BankAccount(0, "+79385635564");
+            acc1.Deposit(100);
+        }
+
+        public static void MessageSending(string phoneNumber, int sum)
+        {
+            Console.WriteLine($"Message send at {phoneNumber}");
+        }
+        public static void AccountCondition(string phoneNumber, int sum)
+        {
+            Console.WriteLine($"Account value {sum}");
+        }
+        public double Rate
+        {
+            get
+            {
+                return _rate;
+            }
+
+            set
+            {
+                var oldRate = _rate;
+                _rate = value;
+                RateChangedEvent?.Invoke(oldRate, _rate);
+            }
+        }
+
+        public event RateChanged RateChangedEvent;
+
+        public void Deposit(int sum)
+        {
+            sum += sum;
+        }
+
+        public void Widthraw(int sum)
+        {
+            sum -= sum;
+        }
+
+        public void RateChange(double newRate)
+        {
+            Rate = newRate;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{_name} {_surname} {_id} {_paymentAccount}");
+        }
+        private static void Main(string[] args)
+        {
+            int a = DateTime.Now.Year;
+            DateTime dayofbrith = new DateTime(16, 11, 1999);
+            Console.WriteLine($"day=(dayofbrith.Day) month=(dayofbrith.Monday) year=(dayofbrith.Year)");
+
+
+
+       }
+
 }
--- 335Labs/Garipova/metods.cs
+++ 335Labs/Garipova/metods.cs
@@ -6,5 +6,34 @@ namespace _335Labs.Garipova
 {
     class metods
     {
+        static void division(double a, int b, int c)
+        {
+            double result = 0;
+            for (int i = b; i <= c; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("Cannot be divided by null");
+                    i = i + 1;
+                }
+                result = a / i;
+                    Console.WriteLine(result);
+            }
+        
+            
+                
+            
+        }
+        
     }
+
 }
+
+                    
+
+
+
+
+
+    
+
