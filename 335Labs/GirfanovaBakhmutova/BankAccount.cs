2cc6eb3949c6d64fcc3e6b94493722e6fd7ef8c0
trunkkker <starstalker19@gmail.com>
2020-11-17 09:20:52 +0300

    also in progress

---
+++ 335Labs/GirfanovaBakhmutova/BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.GirfanovaBakhmutova
+{
+    class BankAccount
+    {
+        public delegate void SumChanging(string phnumber, int sum);
+        public delegate void RateChanging(double OldRate, double NewRate);
+        private int _sum;
+        private static double _rate = 0.35;
+        private DateTime _accountOpenDate;
+        private string _phnumber;
+
+        public BankAccount(int sum, string phnumber)
+        {
+            _sum = sum;
+            _phnumber = phnumber;
+        }
+
+        public int Sum
+        {
+            get
+            {
+                return _sum;
+            }
+            private set
+            {
+                _sum = value;
+                SumChangingEvent?.Invoke(_phnumber, _sum);
+            }
+        }
+        public event SumChanging SumChangingEvent;
+        public string phnumber
+        {
+            get
+            {
+                return phnumber;
+            }
+            private set
+            {
+                _phnumber = value;
+            }
+        }
+        public double Rate
+        {
+            get
+            {
+                return _rate;
+            }
+            set
+            {
+                _rate = value;
+            }
+        }
+        public event RateChanging RateChangingEvent;
+        public void Deposit(int sum)
+        {
+            Sum += sum;
+        }
+        // public void Widthraw(int sum) {}
+    }
+}
--- 335Labs/GirfanovaBakhmutova/Program.cs
+++ 335Labs/GirfanovaBakhmutova/Program.cs
@@ -10,53 +10,8 @@ namespace _335Labs.GirfanovaBakhmutova
     {
         static void Main(string[] args)
         {
-            //MathMetods
-            {
-                // Testing
-
-                // ax^2-bx+c
-                // FindRoot(1, 2, 3, 4);
-
-
-                //c^2=a^2+b^2
-                // Pyth(20, 30);
-
-
-                // Circumference
-                // Circumference(5);
-
-
-                // Circle's area
-                // AreaCirc(5);
-
-
-                // Parallelepiped's area
-                // VolParall(2, 3, 2);
-
-
-                // amperage/voltage/resistance
-                // CurrentCount(0, 5, 15);
-
-
-                // Finds result of division number by an interval of numbers
-                // Division(15, 1, 15);
-                // Console.WriteLine();
-            }
-
-            //StringMetods
-            {
-                // Palindrome
-                // bool a = Palindrome("akaka");
-                // Console.WriteLine(a);
-
-                //EvenOdd("HelloThere");
-
-            }
-            
-            MathOneArray arrayone = Sort;
-            arrayone = [1, 9, 15, 23, 0, 2, 8];
-
-            //arrayone = ([1, 9, 15, 23, 0, 2, 8]);
         }
     }
 }
+    
+
