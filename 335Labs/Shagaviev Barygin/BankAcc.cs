87ba3118d5ea4678d60f9479b72e1e0e12b91bf4
Ibrahim329 <mrshagaviev@gmail.com>
2020-11-17 09:25:17 +0300

    Finish

--- 335Labs/Shagaviev Barygin/BankAcc.cs
+++ 335Labs/Shagaviev Barygin/BankAcc.cs
@@ -12,5 +12,36 @@ namespace _335Labs.Shagaviev_Barygin
         private static double _rate = 0.35;
         private DateTime _accountOpenDate;
         private String _phoneNumber;
+        public BankAcc(int sum, string phoneNumber)
+        {
+            _sum = sum;
+            _phoneNumber = phoneNumber;
+        }
+        public int Sum
+        {
+
+        }
+        public void Deposit(int sum)
+        {
+            Sum +=;
+        }
+
+        public void Withdraw(int sum)
+        {
+            Sum -= sum;
+        }
+        public void ShowProfit(DateTime timeNow)
+        {
+            _sum += _sum * _rate;
+        }
+        public static void RateChanged(double newRate)
+        {
+            _rate = newRate;
+        }
+        
+            
+           
+    
+
     }
 }
