300f52290c09db3a7721242e28088a07724a7966
Ibrahim329 <mrshagaviev@gmail.com>
2020-11-17 08:49:46 +0300

    Start

---
+++ 335Labs/Shagaviev Barygin/BankAcc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Shagaviev_Barygin
+{
+    class BankAcc
+    {
+        public delegate void SumChanged(string phonenumber, int sum);
+        public delegate void RateChanged(double oldRate, double newRate);
+        private int _sum;
+        private static double _rate = 0.35;
+        private DateTime _accountOpenDate;
+        private String _phoneNumber;
+    }
+}
