a7e0407806da0b2e156590e1a358fe808f969d05
aigir <5047098196@edu.tatar.tu>
2020-11-19 22:59:34 +0300

    Strategy(continuing work 2)

--- 335Labs/Girfanova/Strategy/Game.cs
+++ 335Labs/Girfanova/Strategy/Game.cs
@@ -1,102 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static _335Labs.Girfanova.Strategy.I_Health;
+using static _335Labs.Girfanova.Strategy.I_Defence;
+using static _335Labs.Girfanova.Strategy.I_Shooting;
 
 namespace _335Labs.Girfanova.Strategy
 {
     class Game
     {
-        interface IHealth
-        {
-            public int hp { get; set; }
-        }
-        interface IDefence
-        {
-            public int def { get; set; }
-        }
-
-        interface IBow
-        {
-            public int bow { get; set; }
-        }
-
-        sealed class Warrior : IHealth
-        {
-            public int _hp;
-
-            public int hp
-            {
-                get => _hp;
-                set => _hp = value;
-            }
-
-            public int _def;
-
-            public int def
-            {
-                get => _def;
-                set => _def = value;
-            }
-        }
-
-
-        sealed class Pacific : IHealth
-        {
-            public int _hp;
-
-            public int hp
-            {
-                get => _hp;
-                set => _hp = value;
-            }
-
-            public int _def;
-
-            public int def
-            {
-                get => _def;
-                set => _def = value;
-            }
-        }
-
-
-        sealed class WarriorTower : IHealth
-        {
-            public int _hp;
-
-            public int hp
-            {
-                get => _hp;
-                set => _hp = value;
-            }
-
-            public int _def;
-
-            public int def
-            {
-                get => _def;
-                set => _def = value;
-            }
-        }
-
-
-        sealed class PacificTower : IHealth
-        {
-            public int _hp;
-
-            public int hp
-            {
-                get => _hp;
-                set => _hp = value;
-            }
-
-            public int _def;
-
-            public int def
-            {
-                get => _def;
-                set => _def = value;
-            }
-        }
+        
     }
 }
---
+++ 335Labs/Girfanova/Strategy/I_Health.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Girfanova.Strategy
+{
+    public class I_Health
+    {
+        public interface IHealth
+        {
+            public int hp { get; set; }
+        }
+    }
+}
