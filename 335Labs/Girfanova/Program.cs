76b483322614676d4540a7beb1b74498d1cc7e4f
aigir <5047098196@edu.tatar.tu>
2020-11-19 22:48:46 +0300

    Strategy(continuing work)

--- 335Labs/Girfanova/Program.cs
+++ 335Labs/Girfanova/Program.cs
@@ -10,40 +10,7 @@ namespace _335Labs.Girfanova
 
         static void Main(string[] args)
         {
-            // Testing MathMetods
-
-            // ax^2-bx+c
-            //FindRoot(1, 2, 3, 4);
-
-
-            //c^2=a^2+b^2
-            // Pyth(20, 30);
-
-
-            // Circumference
-            // Circumference(5);
-
-
-            // Circle's area
-            // AreaCirc(5);
-
-
-            // Parallelepiped's area
-            // VolParall(2, 3, 2);
-
-
-            // amperage/voltage/resistance
-            // CurrentCount(0, 5, 15);
-
-
-            // Finds result of division number by an interval of numbers
-            // Division(15, 1,15);
-            //Console.ReadLine();
-            //________________________________________________________________
-
-
-            MathOA array = SortOA;
-            array = [1, 0, 16, 9, 77 ];
+           
         }
     }
 }
---
+++ 335Labs/Girfanova/Strategy/Game.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Girfanova.Strategy
+{
+    class Game
+    {
+        interface IHealth
+        {
+            public int hp { get; set; }
+        }
+        interface IDefence
+        {
+            public int def { get; set; }
+        }
+
+        interface IBow
+        {
+            public int bow { get; set; }
+        }
+
+        sealed class Warrior : IHealth
+        {
+            public int _hp;
+
+            public int hp
+            {
+                get => _hp;
+                set => _hp = value;
+            }
+
+            public int _def;
+
+            public int def
+            {
+                get => _def;
+                set => _def = value;
+            }
+        }
+
+
+        sealed class Pacific : IHealth
+        {
+            public int _hp;
+
+            public int hp
+            {
+                get => _hp;
+                set => _hp = value;
+            }
+
+            public int _def;
+
+            public int def
+            {
+                get => _def;
+                set => _def = value;
+            }
+        }
+
+
+        sealed class WarriorTower : IHealth
+        {
+            public int _hp;
+
+            public int hp
+            {
+                get => _hp;
+                set => _hp = value;
+            }
+
+            public int _def;
+
+            public int def
+            {
+                get => _def;
+                set => _def = value;
+            }
+        }
+
+
+        sealed class PacificTower : IHealth
+        {
+            public int _hp;
+
+            public int hp
+            {
+                get => _hp;
+                set => _hp = value;
+            }
+
+            public int _def;
+
+            public int def
+            {
+                get => _def;
+                set => _def = value;
+            }
+        }
+    }
+}
