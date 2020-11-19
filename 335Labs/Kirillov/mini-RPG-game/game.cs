b83fdf029e17a3cb66d5636bf268478e29572a9f
alkir202 <user12367642@gmail.com>
2020-11-19 13:20:42 +0300

    test commit

---
+++ 335Labs/Kirillov/mini-RPG-game/game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Kirillov.mini_RPG_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
