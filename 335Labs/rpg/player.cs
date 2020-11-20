6e35e9d1ced4f3a056e5edc90f78446351f4dd1e
alkir202 <user12367642@gmail.com>
2020-11-20 09:03:57 +0300

    тест

---
+++ 335Labs/rpg/player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.rpg
+{
+    public class Player
+    {
+        private int _health,
+            _maxHealth,
+            _power,
+            _money;
+
+
+        public int Health
+        {
+            get { return _health; }
+        }
+
+        public int MaxHealth
+        {
+            get { return _maxHealth; }
+        }
+
+        public int Power
+        {
+            get { return _power; }
+        }
+
+        public int Money
+        {
+            get { return _money; }
+        }
+
+    }
+}
