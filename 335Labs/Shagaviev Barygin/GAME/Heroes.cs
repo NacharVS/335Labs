47ba551b3041670cc4810af82fbb33d7127f800b
Ibrahim329 <mrshagaviev@gmail.com>
2020-11-27 09:22:45 +0300

    seredina

--- 335Labs/Shagaviev Barygin/GAME/Heroes.cs
+++ 335Labs/Shagaviev Barygin/GAME/Heroes.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace _335Labs.Shagaviev_Barygin.GAME
 {
     class Heroes
     {
+        public int Power { get; set; }
+        public int Health { get; set; }
+
+        public bool dead;
+
+        public Heroes(int power, int health)
+        {
+            Power = power; //сила
+            Health = health; //здоровье
+        }
+
 
     }
 }
--- 335Labs/Shagaviev Barygin/Program1.cs
+++ 335Labs/Shagaviev Barygin/Program1.cs
@@ -2,20 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _335Labs.Shagaviev_Barygin
+namespace _335Labs.Shagaviev_Barygin.GAME
 {
     class Program1
     {
         static void Main()
         {
-            BankAcc Acc1 = new BankAcc(0, "+79600410599");
-            Acc1.Deposit(0);
-            //MessageSending(Acc1.PhoneNumber, Acc1.Sum);
-            //AccountCondition(Acc1.PhoneNumber, Acc1.Sum);
-        }
-        public static void MessageSending(string phoneNumber, int sum)
-        {
-            //Console.WriteLine
+            каменьножницыбумага.игрушка();
         }
 
     
