39c7c7cfbf1c96929f3b51fb24fd88c0f4286f96
MarV <351806@mck-ktits.ru>
2020-11-27 09:30:24 +0300

    w3ew

---
+++ 335Labs/Gayfullin/Game.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _335Labs.Gayfullin
+{
+    internal interface IMovable
+
+    {
+        public double speed { get; }
+        void Move(double speed);
+        void Move();
+    }
+    internal interface IMovable1
+
+    {
+        public double speed { get; set; }
+        public double damage { get; set; }
+        public double armor { get; set; }
+        void Move(double speed);
+        void Move();
+    }
+
+    internal class Fermer : IMovable
+
+    {
+        public double speed => throw new NotImplementedException();
+
+        static void Main(string[] args)
+        {
+            IMovable movableObject;
+
+            movableObject = new Fermer();
+            movableObject.speed = 10;
+            movableObject.Move();
+            
+            movableObject = new Fermer();
+            movableObject.speed = 10;
+            movableObject.Move();
+            
+            movableObject = new Fermer();
+            movableObject.speed = 10;
+            movableObject.Move();
+            
+            movableObject = new Fermer();
+            movableObject.speed = 10;
+            movableObject.Move();
+
+            movableObject = new Fermer();
+            movableObject.speed = 10;
+            movableObject.Move();
+
+
+        }
+
+        internal class Ohranick: IMovable1
+            static void Main(string[] args)
+        {
+            IMovable1 movable1Object;
+
+        movable1Object = new Ohranick();
+        movable1Object.speed = 10;
+            movable1Object.damage = 20;
+            movable1Object.armor = 10;
+            movable1Object.Move();
+            
+            movable1Object = new Ohranick();
+        movable1Object.speed = 10;
+            movable1Object.damage = 20;
+            movable1Object.armor = 10;
+            movable1Object.Move();
+            
+            movable1Object = new Ohranick();
+        movable1Object.speed = 10;
+            movable1Object.damage = 20;
+            movable1Object.armor = 10;
+            movable1Object.Move();
+            
+            movable1Object = new Ohranick();
+        movable1Object.speed = 10;
+            movable1Object.damage = 20;
+            movable1Object.armor = 10;
+            movable1Object.Move();
+
+            movable1Object = new Ohranick();
+        movable1Object.speed = 10;
+            movable1Object.damage = 20;
+            movable1Object.armor = 10;
+            movable1Object.Move();
+
+
+        }
+
+        public void Move(double speed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+   
+}
---
+++ 335Labs/Gayfullin/Банк.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace _335Labs.Gayfullin
+{
+    class MathMetods
+    {
+        delegate void Message();
+        private static void Main(string[] arg)
+        {
+            Console.WriteLine("Добрый день, введите ID-номер вашего счета и выберите операцию: 1 - снять со счета. 2 - положить на счет. 3 - посмотреть информацию о счете. 4 - депозит.");
+
+            int q = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите номер операции:");
+            int a = int.Parse(Console.ReadLine());
+            int b = 40000;
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Укажите сумму, которую вы хотите снять со счета:");
+                    int c = int.Parse(Console.ReadLine());
+                    if (c < 5000)
+                        b = b - c;
+                    break;
+                case 2:
+                    Console.WriteLine("Укажите сумму, которую вы хотите положить на счет:");
+                    int v = int.Parse(Console.ReadLine());
+                    if (v < 12000)
+                        b = b + v;
+                    break;
+                case 3:
+                    {
+
+                        {
+                            Message mesDelegate = () =>
+                            Console.WriteLine("Информация о вашем счете представлена выше^");
+                            Console.WriteLine("ID-номер счета :");
+                            Console.WriteLine($"{q}");
+                            Console.WriteLine("Сумма денег на счету:");
+                            Console.WriteLine($"{b}");
+                            mesDelegate();
+                        }
+                        break;
+
+                    }
+                case 4:
+                    Console.WriteLine("Введите сумму депозита(не более 20000, не менее 4000:");
+                    int g = int.Parse(Console.ReadLine());
+                    break;
+            }
+        }
+
+
+
+    }
+}
