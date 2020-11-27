76be57e9df2ced17e7e60b5f46b0c71d3d168da2
MarV <351806@mck-ktits.ru>
2020-11-27 09:30:04 +0300

    re

--- 335Labs/Solodov/Test.cs
+++ 335Labs/Solodov/Test.cs
@@ -7,19 +7,30 @@ namespace _335Labs.Gayfullin
 {
     class MathMetods
     {
+        delegate void Message();
+        public static void Sum(int b)
+        {
+            Console.WriteLine("Укажите сумму, которую вы хотите снять со счета:");
+            int c = int.Parse(Console.ReadLine());
+            if (c < 5000)
+                b = b - c;
+
+        }
+
         static void Main(string[] arg)
         {
             Console.WriteLine("Добрый день, введите ID-номер вашего счета и выберите операцию: 1 - снять со счета. 2 - положить на счет. 3 - посмотреть информацию о счете. 4 - депозит.");
-            int a = int.Parse(Console.ReadLine());
+
             int q = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите номер операции:");
+            int a = int.Parse(Console.ReadLine());
             int b = 40000;
             switch (a)
             {
+
+
                 case 1:
-                    Console.WriteLine("Укажите сумму, которую вы хотите снять со счета:");
-                    int c = int.Parse(Console.ReadLine());
-                    if (c < 5000)
-                        b = b - c;
+                    Sum(b);
                     break;
                 case 2:
                     Console.WriteLine("Укажите сумму, которую вы хотите положить на счет:");
@@ -28,17 +39,26 @@ namespace _335Labs.Gayfullin
                         b = b + v;
                     break;
                 case 3:
-                    Console.WriteLine("Информация о вашем счете:");
-                    Console.WriteLine("ID-номер счета :");
-                    Console.WriteLine($"{q}");
-                    Console.WriteLine("Сумма денег на счету:");
-                    Console.WriteLine($"{b}");
-                    break;
+                    {
+
+                        {
+                            Message mesDelegate = () =>
+                            Console.WriteLine("Информация о вашем счете представлена выше^");
+                            Console.WriteLine("ID-номер счета :");
+                            Console.WriteLine($"{q}");
+                            Console.WriteLine("Сумма денег на счету:");
+                            Console.WriteLine($"{b}");
+                            mesDelegate();
+                        }
+                        break;
+
+                    }
                 case 4:
                     Console.WriteLine("Введите сумму депозита(не более 20000, не менее 4000:");
                     int g = int.Parse(Console.ReadLine());
                     break;
             }
+
         }
 
 
